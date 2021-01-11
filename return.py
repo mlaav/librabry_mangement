a49db6670213b8f09c8317d5284b4efe33cb1751
karan rathod <42406137+mlaav@users.noreply.github.com>
2021-01-11 18:25:22 +0530

    Add files via upload

--- return.py
+++ return.py
@@ -1,18 +1,19 @@
 from tkinter import *
 from PIL import ImageTk,Image
 from tkinter import messagebox
-import pymysql
-mypass = "root"
-mydatabase="db"
-
-con = pymysql.connect(host="localhost",user="root", password=mypass,database=mydatabase)
-cur = con.cursor()
-
+import psycopg2
+#connectting to the server
+con=psycopg2.connect(
+    database='library',user='postgres',password='toor369',
+     host='127.0.0.1', port= '5432'
+)
+cur=con.cursor()
 # Enter Table Names here
 issueTable = "books_issued" 
 bookTable = "books"
 
 allBid = []  #To store all the Book ID’s
+
 def returnn():
     
     global SubmitBtn,labelFrame,lb1,bookInfo1,quitBtn,root,Canvas1,status
@@ -67,42 +68,3 @@ def returnn():
     
     allBid.clear()
     root.destroy()
-    def returnBook(): 
-    
-        global bookInfo1,SubmitBtn,quitBtn,Canvas1,con,cur,root,labelFrame, lb1
-    
-    root = Tk()
-    root.title("Library")
-    root.minsize(width=400,height=400)
-    root.geometry("600x500")
-
-
-    Canvas1 = Canvas(root)
-    
-    Canvas1.config(bg="#006B38")
-    Canvas1.pack(expand=True,fill=BOTH)
-        
-    headingFrame1 = Frame(root,bg="#FFBB00",bd=5)
-    headingFrame1.place(relx=0.25,rely=0.1,relwidth=0.5,relheight=0.13)
-        
-    headingLabel = Label(headingFrame1, text="Return Book", bg='black', fg='white', font=('Courier',15))
-    headingLabel.place(relx=0,rely=0, relwidth=1, relheight=1)
-    
-    labelFrame = Frame(root,bg='black')
-    labelFrame.place(relx=0.1,rely=0.3,relwidth=0.8,relheight=0.5)   
-        
-    # Book ID to Delete
-    lb1 = Label(labelFrame,text="Book ID : ", bg='black', fg='white')
-    lb1.place(relx=0.05,rely=0.5)
-        
-    bookInfo1 = Entry(labelFrame)
-    bookInfo1.place(relx=0.3,rely=0.5, relwidth=0.62)
-    
-    #Submit Button
-    SubmitBtn = Button(root,text="Return",bg='#d1ccc0', fg='black',command=returnn)
-    SubmitBtn.place(relx=0.28,rely=0.9, relwidth=0.18,relheight=0.08)
-    
-    quitBtn = Button(root,text="Quit",bg='#f7f1e3', fg='black', command=root.destroy)
-    quitBtn.place(relx=0.53,rely=0.9, relwidth=0.18,relheight=0.08)
-    
-    root.mainloop()
